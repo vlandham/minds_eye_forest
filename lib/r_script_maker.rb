eddc3cdb8cdcb0982ea16e786a9fd852c346daca
Jim V <landham@gmail.com>
2008-09-28 14:47:28 -0500

    added r script maker class.  abstracts creation of r scripts a bit

---
+++ lib/r_script_maker.rb
@@ -0,0 +1,36 @@
+class RScriptMaker
+  attr_reader :name
+  def initialize(filename)
+    @script = File.new(filename,"w")
+    @name = filename
+  end
+  
+  def library(lib_name)
+    @script << "library(\'#{lib_name}\')\n"
+  end
+  
+  def load_matrix(matrix_name, matrix_file, rows, cols)
+    @script << "#{matrix_name} = matrix(scan(\'#{File.expand_path(matrix_file)}\', n=#{rows*cols}),"
+    @script << " #{rows}, #{cols}, byrow = TRUE)\n"
+  end
+  
+  def load(file)
+    @script << "load(file=\'#{File.expand_path(file)}\')\n"
+  end
+  
+  def save_matrix(matrix_name, filename)
+    @script << "write.table(#{matrix_name}, file=\"#{File.expand_path(filename)}\" )\n"
+  end
+  
+  def command(command)
+    @script << command.to_s+"\n"
+  end
+  
+  def execute
+    `r CMD BATCH #{File.expand_path(@name)} #{File.expand_path(@name)}out`
+  end
+  
+  def close
+    @script.close
+  end
+end
