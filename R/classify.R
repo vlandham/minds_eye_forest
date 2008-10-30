303973f2bcf14a6f7e4d392b01e170e40d3ffa0b
vlandham <landham@gmail.com>
2008-10-29 23:42:06 -0500

    fixed up the test code a bit -so it should work

--- R/classify.R
+++ R/classify.R
@@ -2,7 +2,7 @@
 #   images_folder -- contains the name of folder of images we're going to classify
 #   forests_folder -- contains the name of the folder containing the group of forests we're going to use
 #   r_directory -- the base folder for the R scripts used in this script
-#   results_file --  file to store the results for the classification
+#   results_folder --  folder to store the results for the classification
 setwd(r_directory)
 # print(results_folder)
 # print(forests_folder)
--- R/test.R
+++ R/test.R
@@ -2,12 +2,12 @@
 #   images_folder -- contains the name of folder of images we're going to classify
 #   forests_folder -- contains the name of the folder containing the group of forests we're going to use
 #   r_directory -- the base folder for the R scripts used in this script
-#   types -- the .dat file that contains the sample classification
+#   results_folder --  folder to store the results for the classification
 
 # print(images_folder)
 # print(forests_folder)
 
 setwd(r_directory)
 
-library('randomForest')
-# library('EBImage')
+source('startup.R')
+source('get_forests.R')
